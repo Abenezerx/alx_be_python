d278b160c1e4bcf929c4cf8b3d6a4eb35da7f02b
Abenezerx <abenu.aa@gmail.com>
2024-12-17 22:45:33 +0300

    simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define the variable (principal in $, rate in %, time in years)
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculation
+interest = principal * rate * time
+
+#print the resut
+print('The simple interest is:', interest)
