5a919c5b429cdd63a9340433259d6e24fc19af46
Abenezerx <abenu.aa@gmail.com>
2024-12-17 23:05:57 +0300

    modify hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,9 +1,8 @@
 #define variable
 hours = 2
-one_second = 3600
 
 #calculation
-seconds = hours * one_second
+seconds = hours * 3600
 
 #print results
 print(hours, 'hour(s) is', seconds, 'seconds.')
