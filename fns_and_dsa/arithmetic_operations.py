be12cc4055cc8dd9275e46e44a997d365333fd10
Abenezerx <abenu.aa@gmail.com>
2025-01-17 23:59:17 +0300

    modify arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,3 +12,10 @@ def perform_operation(num1, num2, operation):
             print('Error. Division by zero is not allowed!')
     else:
         print('Error. You used an incorrect operation.')
+
+if __name__ == "__main__": 
+    num1 = 5.0 
+    num2 = 0.0 
+    operation = "divide" 
+    result = perform_operation(num1, num2, operation) 
+    print(f"Result: {result}")
