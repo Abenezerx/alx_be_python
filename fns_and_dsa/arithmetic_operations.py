1ead9eaa8810bb3900a53fbfd7a4b37f7c7ae6e8
Abenezerx <abenu.aa@gmail.com>
2025-01-18 00:02:32 +0300

    modify arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,16 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
-            return num1/num2
-        else:
+        if num2 == 0:
             print('Error. Division by zero is not allowed!')
+        else:
+            return num1/num2
     else:
         print('Error. You used an incorrect operation.')
-
-if __name__ == "__main__": 
-    num1 = 5.0 
-    num2 = 0.0 
-    operation = "divide" 
-    result = perform_operation(num1, num2, operation) 
-    print(f"Result: {result}")
