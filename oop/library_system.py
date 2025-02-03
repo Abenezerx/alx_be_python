fb9aa117458013e35bf50c64f39c1320adda19b9
Abenezerx <abenu.aa@gmail.com>
2025-02-03 22:13:05 +0300

    add library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class EBook(Book):
+    def __init__(self, file_size):
+        self.fie_size = file_size
+        super().__init__(title, author)
+
+class PrintBook(Book):
+    def __init__(self, page_count):
+        self.page_count = page_count
+        super().__init__(title, author)
+
+class Library:
+    def __init__(self):
+        self.books[]
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.list_books:
+            print(book)
